c482590475ad40493d89027df7129cad3eb55b97
SuJeet Mehra <82982529+sujeetmehra01@users.noreply.github.com>
2023-12-29 09:31:45 +0530

    Authentication login/register

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,16 +1,49 @@
-import React from "react";
+import { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
-const LoginPage = () => {
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
+  async function login(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    if (response.ok) {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
+    } else {
+      alert("wrong credentials");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
   return (
-    <>
-      <form className="login">
-        <h1>Login</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </>
+    <form className="login" onSubmit={login}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>Login</button>
+    </form>
   );
-};
-
-export default LoginPage;
+}
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const [username, SetUsername] = useState("");
+  const [password, SetPassword] = useState("");
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((res) => {
+      if (res.status === 200) {
+        alert("User registered successfully");
+        window.location.href = "/login";
+      } else {
+        alert("Registration failed");
+      }
+    });
+  }
   return (
     <>
-      <form className="register">
+      <form className="register" onSubmit={register}>
         <h1>Register</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(ev) => SetUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(ev) => SetPassword(ev.target.value)}
+        />
         <button type="submit">Register</button>
       </form>
     </>
